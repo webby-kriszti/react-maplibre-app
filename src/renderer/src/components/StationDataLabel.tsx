8d162b617ab3792184bf4062b9c69e0fd287c5f7
webby-kriszti <webby.kriszti@gmail.com>
2026-06-11 11:27:07 +0200

    install zeromq

--- src/renderer/src/components/StationDataLabel.tsx
+++ src/renderer/src/components/StationDataLabel.tsx
@@ -1,3 +1,4 @@
+import { ReactElement } from 'react'
 import { Station } from 'src/shared/types'
 
 type BaseProp = {
@@ -6,8 +7,37 @@ type BaseProp = {
 type CompactProp = BaseProp & {
   mode: 'compact'
 }
+type TooltipInfo = {
+  title: string
+  topic: string
+}
 type DetailedProp = BaseProp & {
   mode: 'detailed'
-  tooltipInfo: { title: string; topic: string }
+  tooltipInfo: TooltipInfo
 }
 type StationDataLabelProps = CompactProp | DetailedProp
+export const StationDataLabel = (props: StationDataLabelProps): ReactElement => {
+  const { mode, station } = props
+  if (mode === 'compact') {
+    return (
+      <div>
+        <p>name: {station.name}</p>
+        <p>{`Coordinates: ${station.coordinates[0]}, ${station.coordinates[1]}`}</p>
+      </div>
+    )
+  } else {
+    const { tooltipInfo } = props
+    const handleClick = (): void => {
+      console.log(`opening help for topic: ${tooltipInfo.topic}`)
+    }
+    return (
+      <div>
+        <p>name: {station.name}</p>
+        <p>{`Coordinates: ${station.coordinates[0]}, ${station.coordinates[1]}`}</p>
+        <div onClick={handleClick} title={tooltipInfo.title}>
+          <p>🪲</p>
+        </div>
+      </div>
+    )
+  }
+}
